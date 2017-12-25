37de0f94c790e5aa8fff28d2a6c7f9a6d8a75f33
Jose Sanchez <jparkers0012@gmail.com>
2017-12-25 12:28:54 -0500

    integrate Slack RTM for messaging, WIT for natural language processing

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,25 @@
 'use strict';
 
+require('dotenv').config();
+
+const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
-
 const server = http.createServer(service);
 
-server.listen(3000);
+const witToken = process.env.WIT_API_TOKEN;
+const witClient = require('../server/witClient')(witToken);
+
+const slackToken = process.env.SLACK_BOT_API_TOKEN;
+const slackLogLevel = 'verbose';
+
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
+rtm.start();
+
+slackClient.addAuthenticatedHandler(rtm, () => {
+  server.listen(3000);
+})
+
 
 server.on('listening', () => {
   console.log(`Vee is listening on ${server.address().port} in ${service.get('env')}`);
---
+++ server/slackClient.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
+let nlp = null;
+
+function handleOnAuthenticated(rtmStartData){
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name},
+     but not yet connected to a channel`);
+}
+
+function handleOnMessage(message){
+  nlp.ask(message.text);
+  rtm.sendMessage("Hello there", message.channel, function messageSent(){
+
+  });
+}
+function addAuthenticatedHandler(rtm, handler){
+  rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
+
+module.exports.init = function slackClient(token, logLevel, nlpClient){
+  rtm = new RtmClient(token, {logLevel: logLevel});
+  nlp = nlpClient;
+  addAuthenticatedHandler(rtm, handleOnAuthenticated);
+  rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
+  return rtm;
+}
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;

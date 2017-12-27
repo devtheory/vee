ea30c66fff5da4db3b3dd9f0cc155ddab72165f9
Jose Sanchez <jparkers0012@gmail.com>
2017-12-27 01:23:34 -0500

    refactor intents for dynamic intents

--- bin/run.js
+++ bin/run.js
@@ -1,25 +1,25 @@
 'use strict';
 
+//environment variables
 require('dotenv').config();
+const slackToken = process.env.SLACK_BOT_API_TOKEN;
+const witToken = process.env.WIT_API_TOKEN;
 
-const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
-const witToken = process.env.WIT_API_TOKEN;
+//clients
+const slackClient = require('../server/slackClient');
 const witClient = require('../server/witClient')(witToken);
 
-const slackToken = process.env.SLACK_BOT_API_TOKEN;
 const slackLogLevel = 'verbose';
-
 const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => {
   server.listen(3000);
 })
-
 
 server.on('listening', () => {
   console.log(`Vee is listening on ${server.address().port} in ${service.get('env')}`);
--- server/slackClient.js
+++ server/slackClient.js
@@ -12,25 +12,45 @@ function handleOnAuthenticated(rtmStartData){
 }
 
 function handleOnMessage(message){
-  nlp.ask(message.text, (err, res) => {
-    if(err){
-      console.log(err);
-      return;
-    }
+  if(message.text.toLowerCase().includes('vee')){
+    nlp.ask(message.text, (err, res) => {
+      const intentType = res.intent && res.intent[0];
+      console.log(`intentType is ${intentType.value}`)
+      if(err){
+        console.log(err);
+        return;
+      }
 
-    if(!res.intent){
-      return rtm.sendMessage("Sorry, I don't understand", message.channel);
-    } else if(res.intent[0].value == 'time' && res.location){
-      return rtm.sendMessage(`I don't yet know the time in ${res.location[0].value}`,message.channel)
-    } else {
-      console.log(res);
-      return rtm.sendMessage("Sorry, I don't understand", message.channel);
-    }
+      try {
+        if(!res.intent || !intentType || intentType.value){
+          throw new Error('Could not extract intent');
+        }
+        const intent = require(`./intents/${intentType.value}Intent`);
 
-    rtm.sendMessage("Sorry, I didn't get that.", message.channel, function messageSent(){
+        intent.process(res, (error, response) => {
+          if(error){
+            console.log(error.message);
+            return;
+          }
 
-    });
-  });
+          return rtm.sendMessage(response, message.channel);
+        })
+      }catch(err){
+        console.log(err);
+        console.log(res);
+        rtm.sendMessage("Sorry, I don't know what you mean", message.channel);
+      }
+
+      if(!res.intent){
+        return rtm.sendMessage("Sorry, I don't understand", message.channel);
+      } else if(intentType.value == 'time' && res.location){
+        return rtm.sendMessage(`I don't yet know the time in ${intentType.value}`,message.channel);
+      } else {
+        console.log(res);
+        return rtm.sendMessage("Sorry, I don't understand", message.channel);
+      }
+    })
+  };
 }
 function addAuthenticatedHandler(rtm, handler){
   rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);

aa1155db7ab274a306ff668da926738e29bbbd1c
Jose Sanchez <jparkers0012@gmail.com>
2017-12-27 23:30:13 -0500

    implement service registry

---
+++ server/serviceRegistry.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class ServiceRegistry {
+  constructor(){
+    this._services = [];
+    this._timeout = 30; //remove services that don't announce themselves after timeout
+  }
+
+  add(intent, ip, port){
+    const key = intent+ip+port;
+    if(!this._services[key]){ //if service doesn't exist, register it
+      let obj = {intent, ip, port, timestamp: Math.floor(new Date() / 1000)};
+      this._services[key] = obj;
+      console.log(`Added service for intent ${intent} on ${ip}:${port}`);
+      this._cleanup();
+      return;
+    }
+
+    //if service exists, update timestamp
+    this._services[key].timestamp = Math.floor(new Date() / 1000);
+    console.log(`Updated service for intent ${intent} on ${ip}:${port}`);
+    this._cleanup();
+  }
+
+  remove(intent, ip, port){
+    const key = intent+ip+port;
+    delete this._services[key];
+  }
+
+  get(intent){
+    this._cleanup();
+    return this._services.find(service => intent == service.intent);
+  }
+
+  _cleanup(){
+    const now = Math.floor(new Date() / 1000);
+
+    for(let key in this._services){
+      if(this._services[key].timestamp + this._timeout < now){//expired service, remove it
+        delete this._services[key];
+      }
+    }
+  }
+}
+
+module.exports = ServiceRegistry;
